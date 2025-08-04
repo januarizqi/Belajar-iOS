8613eddbd85fe938ba93b688ce3f336966f02f3a
Januarizqi Dwi Milleniantoro <januarrizqi5@gmail.com>
2025-08-04 15:28:17 +0700

    create folder “Diobatin” app logo

---
+++ Logo/Logo/LogoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogoApp.swift
+//  Logo
+//
+//  Created by Januarizqi Dwi Milleniantoro on 04/08/25.
+//
+
+import SwiftUI
+
+@main
+struct LogoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
